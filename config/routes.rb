2515ea7f400e341060cdcf7630f1c2a73561af61
Shinkat0 <cntaro1234@gmail.com>
2020-07-02 19:10:54 +0000

    add login/signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 	Rails.application.routes.draw do
 		devise_for :users
+
 		resources :users,only: [:show,:edit,:update]
 		resources :post_trainings do
 			resources :comments, only: [:create, :destroy]
